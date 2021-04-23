7c52cf0c2e8d017569133fb08fd92841c8c201b9
foudadev <40678747+foudadev@users.noreply.github.com>
2021-04-23 18:22:07 +0200

    udate api.php file more clean code

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,64 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) { return $request->user(); });
-
 // Client Login
-Route::post('register', 'API\ClientAuthController@register');
-Route::post('login', 'API\ClientAuthController@login');
-
-// for update client info by client
+Route::post('client/register', 'API\ClientAuthController@register');
+Route::post('client/login', 'API\ClientAuthController@login');
+//  client CRUD With logout
 Route::middleware('auth:client')->group(function(){
-    Route::post('client/update/{id}','API\ClientInfoController@update');
-    Route::post('logout','API\ClientAuthController@logoutApi');
-
+     Route::apiResource('client','API\ClientInfoController');
+     Route::post('client/logout','API\ClientAuthController@logoutApi');
 });
-
 
 // for nurse login
-Route::post('register', 'API\NurseAuthController@register');
-Route::post('login', 'API\NurseAuthController@login');
-
+Route::post('nurse/register', 'API\NurseAuthController@register');
+Route::post('nurse/login', 'API\NurseAuthController@login');
 // for update nurse info by nurse
 Route::middleware('auth:api')->group(function(){
-    Route::post('nurse/update/{id}','API\NurseInfoController@update');
+    Route::apiResource('nurse','API\NurseInfoController')->only(['update']);
     Route::post('product/upload/{id}','API\NurseInfoController@uploadImage');
     Route::post('logout','API\NurseAuthController@logoutApi');
-
-
 });
 
-Route::group(['prefix' => 'admin'],function (){
-    Route::post('login', 'API\AdminController@login');
-    Route::post('logout','API\AdminController@logoutApi')-> middleware(['auth:admin-api']);
-
-    Route::get('category/index','API\CategoriesController@index')-> middleware(['auth:admin-api']);
-    Route::post('category/store','API\CategoriesController@store')-> middleware(['auth:admin-api']);
-    Route::get('category/show/{id}','API\CategoriesController@show')-> middleware(['auth:admin-api']);
-    Route::post('category/update/{id}','API\CategoriesController@update')-> middleware(['auth:admin-api']);
-    Route::delete('category/delete/{id}','API\CategoriesController@destroy')-> middleware(['auth:admin-api']);
-
-    /////////////////////////////////// products Routes /////////////////////////////////////
-
-    Route::get('product/index','API\ProductsController@index')-> middleware(['auth:admin-api']);
-    Route::post('product/store','API\ProductsController@store')-> middleware(['auth:admin-api']);
-    Route::get('product/show/{id}','API\ProductsController@show')-> middleware(['auth:admin-api']);
-    Route::post('product/update/{id}','API\ProductsController@update')-> middleware(['auth:admin-api']);
-    Route::delete('product/delete/{id}','API\ProductsController@destroy')-> middleware(['auth:admin-api']);
-
-    //////////////////////////////// Nurse Routes //////////////////////////////////////////////////////
-    ////////////////////// this for admin to give information about nurse and insert   //////////////////////////////
-
-    Route::get('nurse/index','API\NurseInfoController@index')-> middleware(['auth:admin-api']);
-    Route::post('nurse/store','API\NurseInfoController@store')-> middleware(['auth:admin-api']);
-    Route::get('nurse/show/{id}','API\NurseInfoController@show')-> middleware(['auth:admin-api']);
-    Route::delete('nurse/delete/{id}','API\NurseInfoController@destroy')-> middleware(['auth:admin-api']);
-
-    ///////////// client routes ////////////////////////////////////////////////////
-
-    Route::get('product/index','API\ClientInfoController@index')-> middleware(['auth:admin-api']);
-    Route::post('product/store','API\ClientInfoController@store')-> middleware(['auth:admin-api']);
-    Route::get('product/show/{id}','API\ClientInfoController@show')-> middleware(['auth:admin-api']);
-    Route::delete('product/delete/{id}','API\ClientInfoController@destroy')-> middleware(['auth:admin-api']);
-
+// admin login
+Route::post('admin/login', 'API\AdminController@login');
+// Admin Control on CRUD category and product and nurse with logout
+Route::group(['prefix' => 'admin','middleware' => 'auth:admin-api'],function (){
+    Route::apiResource('category','API\CategoriesController');
+    Route::apiResource('product','API\ProductsController');
+    Route::apiResource('nurse','API\NurseInfoController')->except(['update']);
+    Route::post('logout','API\AdminController@logoutApi');
 });
